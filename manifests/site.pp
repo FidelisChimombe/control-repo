684151a7c3a06b2f86b666a36b98063598e753aa
FidelisChimombe <FidelisChimombe@users.noreply.github.com>
2019-07-25 00:13:00 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,13 @@ node default {
 #classification for just the master
 node 'master.puppet.vm' {
   include role::master_server
+  file { '/root/README'
+      ensure=>file,
+      content=>$fqdn
+  }
 }
+#using $ for content, will make the puppet master get the fqdn the agent submitted when it first ran
+#facts are just preset variables by puppet
 
 node /^web/ {
   include role::app_server
